ff6c5243ac411bee2d7ce5b6065e6646cde2c453
Mohit Mehta <mohmeh@qti.qualcomm.com>
2026-07-19 22:00:21 -0700

    Add license header to ODS sampling helper
    Signed-off-by: Mohit Mehta <mohmeh@qti.qualcomm.com>

--- vllm_qaic/worker/ods_sampling.py
+++ vllm_qaic/worker/ods_sampling.py
@@ -1,3 +1,7 @@
+# ------------------------------------------------------------------
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+# SPDX-License-Identifier: BSD-3-Clause-Clear
+# ------------------------------------------------------------------
 """On-device sampling array construction helpers.
 
 This module converts vLLM per-request sampling metadata into per-slot NumPy
