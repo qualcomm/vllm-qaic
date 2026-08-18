973c515d9af9047d8b8550e5a024bc39e96d8d9b
Aditya Jadhav <aditjadh@qti.qualcomm.com>
2026-08-17 19:58:06 -0700

    [v0.23.0][Enhancement] Add __main__ guard to qaic_eager_mode example (#67)
    Signed-off-by: Aditya Dhananjay Jadhav <aditjadh@qti.qualcomm.com>

--- examples/qaic_eager_mode.py
+++ examples/qaic_eager_mode.py
@@ -32,30 +32,36 @@ os.environ["QAIC_VISIBLE_DEVICES"] = (
     "0"  # for multiple devices, separate them through comma, i.e. 0,1,2,3
 )
 
-# Create a LLM.
-llm = LLM(
-    model="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
-    max_num_seqs=decode_bsz,  # determines decode batch size
-    max_model_len=ctx_len,  # ctx_len (does not account for padding,
-    # but does account for prompt and generated tokens)
-    enable_prefix_caching=False,
-    gpu_memory_utilization=0.9,
-    enforce_eager=True,
-    async_scheduling=False,
-    long_prefill_token_threshold=seq_len,
-    tensor_parallel_size=1,
-    # buffers
-)
-# Generate texts from the prompts. The output is a list of RequestOutput objects
-# that contain the prompt, generated text, and other information.
-outputs = llm.generate(prompts, sampling_params)
-# Print the outputs.
-for output in outputs:
-    prompt = output.prompt
-    generated_text = output.outputs[0].text
-    generated_tokens = output.outputs[0].token_ids
-    num_generated_tokens = len(generated_tokens)
-    print(
-        f"Prompt: {prompt!r}, Generated text: {generated_text!r},\
-         Num generated tokens: {num_generated_tokens!r}"
+
+def main():
+    # Create a LLM.
+    llm = LLM(
+        model="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+        max_num_seqs=decode_bsz,  # determines decode batch size
+        max_model_len=ctx_len,  # ctx_len (does not account for padding,
+        # but does account for prompt and generated tokens)
+        enable_prefix_caching=False,
+        gpu_memory_utilization=0.9,
+        enforce_eager=True,
+        async_scheduling=False,
+        long_prefill_token_threshold=seq_len,
+        tensor_parallel_size=1,
+        # buffers
     )
+    # Generate texts from the prompts. The output is a list of RequestOutput
+    # objects that contain the prompt, generated text, and other information.
+    outputs = llm.generate(prompts, sampling_params)
+    # Print the outputs.
+    for output in outputs:
+        prompt = output.prompt
+        generated_text = output.outputs[0].text
+        generated_tokens = output.outputs[0].token_ids
+        num_generated_tokens = len(generated_tokens)
+        print(
+            f"Prompt: {prompt!r}, Generated text: {generated_text!r},\
+             Num generated tokens: {num_generated_tokens!r}"
+        )
+
+
+if __name__ == "__main__":
+    main()
