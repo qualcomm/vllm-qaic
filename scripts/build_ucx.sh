429225359412d1c263379fe51ca4ff68c2210204
Varun Gupta <vargupt@qti.qualcomm.com>
2026-09-03 01:23:11 -0700

    Added license header
    Signed-off-by: Varun Gupta <vargupt@qti.qualcomm.com>

--- scripts/build_ucx.sh
+++ scripts/build_ucx.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# ------------------------------------------------------------------
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+# SPDX-License-Identifier: BSD-3-Clause-Clear
+# ------------------------------------------------------------------
 
 set -euxo pipefail
 
